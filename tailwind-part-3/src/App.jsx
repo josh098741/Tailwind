6ce7b9d8bea185764e60dbdf9ff2d46326c9b496
josh098741 <morongemokaya@gmail.com>
2025-09-30 14:41:49 +0300

    Additional commits

--- tailwind-part-3/src/App.jsx
+++ tailwind-part-3/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Typography
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return(
+    <div>
+      <h1 className="text-3xl">Title 1</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Title 3</h3>
+      <p className="text-base">A regular paragraph</p>
+      <p className="text-sm">A description Paragraph</p>
+      <p className="note text-xs">A little Note</p>
+    </div>
+  );
 }
 
 export default App
