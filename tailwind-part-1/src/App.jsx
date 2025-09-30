d4a5792989ceb074728be1a2c017de3947cce615
josh098741 <morongemokaya@gmail.com>
2025-09-30 13:23:32 +0300

    Additional commits

---
+++ tailwind-part-1/src/App.jsx
@@ -0,0 +1,16 @@
+//colors
+
+function App() {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold text-center text-slate-500">Colors in Tailwind</h1>
+            <h1 className="text-cyan-600 border-4 border-green-300">Hello World</h1>
+            <h3 className="text-teal-400">These is how its done my boy</h3>
+            <h2 className="bg-green-500 text-white">Hello world again</h2>
+            <p className="text-red-400 border-8 border-black">First text to begin with</p>
+            <p className="text-vuejs">Custon color in extension</p>
+        </div>
+    );
+}
+
+export default App
