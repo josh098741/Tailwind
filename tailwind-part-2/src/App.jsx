4d6e35ff9a958c2a83a6a94e3802e7a42a0ba605
josh098741 <morongemokaya@gmail.com>
2025-09-30 13:57:16 +0300

    Additional commits

--- tailwind-part-2/src/App.jsx
+++ tailwind-part-2/src/App.jsx
@@ -1,8 +1,18 @@
+//Configuration 
+//  mt- margin top | mb - margin bottom | text-xs - text size extra small 
+//  bg-[#2345678] lets you create custom colors by providing the color picker
 
+//  Plugins extending Tailwind with reusable third party plugins
+//  Plugins let you register new styles for tailwind to inject into the users stylesheet using javascript instead of css
 
 function App() {
   return(
-    <div></div>
+    <div>
+        <h1>Configuration of Tailwind Config</h1>
+        <h1 className="text-xs lg:text-3xl">Hello Long Sentence</h1>
+        <h1 className="text-xs sm:text-base md:text-lg lg:text-3xl">Another Long sentence</h1>
+        <p className="mt-6 bg-[#c3ff00]">Another Another Long Sentence</p>
+    </div>
   );
 }
 
