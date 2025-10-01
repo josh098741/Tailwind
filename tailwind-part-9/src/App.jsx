c9dd1d89580ffb4454358c5f238ef47184466f74
josh098741 <morongemokaya@gmail.com>
2025-10-02 01:32:01 +0300

    Additional commit

--- tailwind-part-9/src/App.jsx
+++ tailwind-part-9/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Effects and Filters
+//shadow || shadow-md || shadow-lg || shadow-xl || shadow-2xl || shadow-inner || shadow-cyan-500/50 => (/50) represents opacity
+
+//Opacity
+//opacity-1 => opacity-100
+
+//blur
+//blur || blur-md || blur-lg || blur-xl || blur-2xl
+
+//brightness
+//brightness-0 || brightness-50 || brightness-75 || brightness-100 || brightness-105
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+  return(
+    <div className="h-screen p-4 Parent">
+      <div className=" inline-block text-center p-4 text-black bg-white border rounded-lg shadow-md">
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This Is My Div</p>
+        <button className="px-3 brightness-200 opacity-10 py-2 bg-cyan-500 rounded-lg cursor-pointer shadow-md shadow-cyan-500/50 text-white">
+          Say Hello
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
