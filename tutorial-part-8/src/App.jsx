bd581bea779ca3eafcc2008edfb303f26b6a7a66
josh098741 <morongemokaya@gmail.com>
2025-10-02 00:55:22 +0300

    Addition of border in tailwind 8

--- tutorial-part-8/src/App.jsx
+++ tutorial-part-8/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  return(
+    <div className="h-screen text-white bg-slate-900 Parent">
+      <header className="w-full border-2 border-l-8  py-4">Header</header>
+      <div className="divide-y divide-slate-200 my-5">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+      </div>
+      <div className="my-5"> 
+        <input type="text" className="border-2 border-rose-600 border-double outline-none"/>
+      </div>
+      <div className="my-5 grid grid-cols-3 divide-x divide-red-500 divide-double">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+      <div className="my-5">
+        <button className="outline mx-4 outline-offset-2 outline-4">Button A</button>
+        <button className="outline mx-5 outline-offset-4 outline-4">Button B</button>
+        <button className="outline outline-offset-8 outline-4 outline-blue-400">Button C</button>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="ring-offset-2 ring-4">Button A</button>
+        <button className="mx-5 hover:ring-offset-2 ring-8">Button B</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
