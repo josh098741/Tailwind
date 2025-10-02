10aaa9a0a57e16462e05b9a00db3a5788949af8d
josh098741 <morongemokaya@gmail.com>
2025-10-02 11:35:10 +0300

    Additional commit of part 10 containing animations

--- tailwind-part-10/src/App.jsx
+++ tailwind-part-10/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Animations and transitions
+import web from './assets/web.jpg'
+//translate => || -translate-y-6 || -translate-x-6
+//  transition => delay-100 (0.1s) || duration-1000 || timing functions => (ease-in-out,ease,ease-in,ease-out)
+//  animation => animate-pulse || animate-spin || animate-ping || animate-bounce
+//  spin => scale-0 || scale-x-0 || scale-y-0 || scale-50 || scale-x-50
+//  rotate => rotate-90
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return(
+    <div className="h-screen p-4 Parent">
+      <div className="inline-block p-4 text-black bg-white border rounded-lg">
+        <img src={web} alt="web" className='w-64 rotate-6 mb-4'/>
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This is demonstration</p>
+        <button className="border px-3 py-2 cursor-pointer hover:-translate-y-2 transition delay-100 duration-1000 rounded-lg bg-cyan-500 shadow-xl shadow-gray-600 text-white">Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+    </div>
+  );
 }
 
 export default App
