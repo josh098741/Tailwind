41443665cafe45d9a0fdd83c476940c6a02efcae
josh098741 <morongemokaya@gmail.com>
2025-10-02 15:41:53 +0300

    Additional commit

--- tailwind-part-11/src/App.jsx
+++ tailwind-part-11/src/App.jsx
@@ -1,6 +1,15 @@
-
+import {useState} from 'react'
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectValue, setSelectValue] = useState("Option 1")
+
+  const updatedValue = (value) => {
+    setSelectValue(value)
+    setIsOpen(false)
+  }
+
   return(
     <div>
       <h1>This is a title</h1>
@@ -19,15 +28,31 @@ function App() {
       <div>
         <input type="text" />
       </div>
-      <div>
+      <div className="flex">
         <input type="checkbox" id="checkbox" />
-        <label htmlFor="checkbox">Checkbox</label>
+        <label htmlFor="checkbox">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita at ad dicta culpa, impedit quam iusto eveniet tempora quasi consectetur officia voluptatibus maxime magnam repudiandae, amet sint enim nulla sapiente!</label>
       </div>
       <div>
         <select>
           <option>Option 1</option>
           <option>Option 2</option>
         </select>
+      </div>
+      <div className="Select">
+        <div className="child" onClick={() => setIsOpen(!isOpen)}>{selectValue}</div>
+        {
+          isOpen && (
+            <div>
+              <ul className="flex flex-col divide-y border-t">
+                <li className="child" onClick={() => updatedValue("Option 1")}>Option 1</li>
+                <li className="child" onClick={() => updatedValue("Option 2")}>Option 2</li>
+                <li className="child" onClick={() => updatedValue("Option 3")}>Option 3</li>
+                <li className="child" onClick={() => updatedValue("Option 4")}>Option 4</li>
+              </ul>
+            </div>
+          )
+        }
+        
       </div>
       <h1>This is another title</h1>
       <h2>This is another subtitle</h2>
